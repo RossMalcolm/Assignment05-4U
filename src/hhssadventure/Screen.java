bcfc7cdd30e219a985b75338bd9a805183b9bab9
farrb0382 <farrb0382@HRH-IC0027343.wrdsb.ca>
2016-11-24 13:54:18 -0500

    Working on project.

--- src/hhssadventure/Screen.java
+++ src/hhssadventure/Screen.java
@@ -15,38 +15,38 @@ import javax.imageio.ImageIO;
  */
 public class Screen {
     //private ints 
+
     private String direction;
     private String hall;
     private BufferedImage image;
-    private boolean cantmove; 
+    private boolean cantmove;
     private String nextHall;
     private String nextDirection;
 
     public Screen(String hall, Scanner input) {
         this.hall = hall;
-        
+
         this.direction = input.next();
-        
+
         String filename = input.next();
-        
+
         this.cantmove = input.nextBoolean();
-        
-        if(cantmove == false){
+
+        if (cantmove == false) {
             this.nextHall = input.next();
             this.nextDirection = input.next();
         }
 
         //load image file
-       try{
-            image = ImageIO.read(new File ("pics/" + filename));
-        }catch(Exception e){
+        try {
+            image = ImageIO.read(new File("images/" + filename));
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        
-       input.nextLine();
-       
+
+        input.nextLine();
+
     }
-    
 
     public String getHall() {
         return this.hall;
@@ -63,22 +63,24 @@ public class Screen {
     public BufferedImage getImage() {
         return this.image;
     }
-    
-    public String getNextHall(){
+
+    public String getNextHall() {
         return this.nextHall;
     }
-    public String getNextDirection(){
+
+    public String getNextDirection() {
         return this.nextDirection;
     }
-    
+
     //test
     public static void main(String[] args) {
-        
+
         Scanner in = new Scanner(System.in);
-        Screen s = new Screen(Upstairs1, in);
+        Screen s = new Screen("Upstairs1", in);
         System.out.println(s.getHall());
         System.out.println(s.move());
         System.out.println(s.getImage());
-    
-    }
+        System.out.println(s.getDirection() + s.getNextHall());
 
+    }
+}
