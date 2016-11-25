8699652492890b6599cdf9e2e0875e85a21d046a
malcr1272 <malcr1272@HRH-IC0027342.wrdsb.ca>
2016-11-25 12:58:43 -0500

    all classes

--- src/hhssadventure/Controller.java
+++ src/hhssadventure/Controller.java
@@ -9,6 +9,20 @@ package hhssadventure;
  * @author malcr1272
  */
 public class Controller {
-private GUI gui;
-private School school;
+
+    private GUI gui;
+    private School school;
+    private String hall;
+    private String direction;
+
+    public Controller(GUI gui, School school, String direction, String hall) {
+    this.gui = gui;
+    this.school = school;
+    this.direction = direction;
+    this.hall = hall;
+    
+    gui.setController(this);
+    
+    Screen start = school.
+    }
 }
